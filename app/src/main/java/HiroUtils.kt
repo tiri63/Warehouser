a691ede769b19218de457bb20aabe95c15e16bd4
super-hiro69 <i@rexio.cn>
2022-09-04 21:15:51 +0800

    Ripple Background, StatusBar Text

---
+++ app/src/main/java/HiroUtils.kt
@@ -0,0 +1,18 @@
+
+import android.content.res.Configuration
+import android.content.res.Resources
+import android.graphics.Color
+import android.view.Window
+import androidx.core.view.WindowCompat
+
+
+class HiroUtils {
+    companion object Factory {
+        fun setStatusBarColor(window : Window, resources : Resources)
+        {
+            val isLight = (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_NO
+            window.statusBarColor = if (isLight) Color.WHITE else Color.BLACK
+            WindowCompat.getInsetsController(window, window.decorView)?.isAppearanceLightStatusBars = isLight
+        }
+    }
+}
--- app/src/main/java/cn/rexio/vc/warehouser/MainActivity.kt
+++ app/src/main/java/cn/rexio/vc/warehouser/MainActivity.kt
@@ -1,17 +1,39 @@
 package cn.rexio.vc.warehouser
 
+import HiroUtils
+import android.app.Activity
+import android.content.res.Configuration
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.view.View
+import android.view.WindowManager
+import androidx.annotation.RequiresApi
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        WindowCompat.setDecorFitsSystemWindows(window, false)
+        actionBar.let { it?.hide() }
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
         super.onCreate(savedInstanceState)
+        window.statusBarColor = Color.TRANSPARENT
         setContentView(R.layout.main_layout)
-        supportActionBar.let { it?.hide() }
+        setStatusBarColor()
         var ui_scroll_main = findViewById<ConstraintLayout>(R.id.ui_scroll_main)
     }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        this.setStatusBarColor()
+        super.onConfigurationChanged(newConfig)
+    }
+
+    private fun setStatusBarColor()
+    {
+        HiroUtils.setStatusBarColor(window,resources)
+    }
+
 }
